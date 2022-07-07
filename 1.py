bc26c5783fa3408bc33395e81cd4d358dbeb021e
MoonSanghee <psycho13th@gmail.com>
2022-07-08 06:24:32 +0900

    백준 1-12까지 클리어

--- 1.py
+++ 1.py
@@ -19,3 +19,22 @@
 
 # A, B = input().split()
 # print(int(A)-int(B))
+
+# A,B = map(int,input().split())
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(A/B)
+# print(A%B)
+# 24-28은 print(A+B, A-B, A*B, A//B, A%B, sep='\n')	으로도 표현 가능
+# sep'\n'으로 줄바꿈.
+
+# a = int(input())
+# print(a -2541 + 1998)
+
+A,B,C = map(int,input().split())
+
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
