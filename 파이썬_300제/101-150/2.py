0d5edf23a71b4a261a99bfd75c15f922e5cf30ab
MoonSanghee <psycho13th@gmail.com>
2022-07-08 09:19:22 +0900

    120까지 완

--- 파이썬_300제/101-150/2.py
+++ 파이썬_300제/101-150/2.py
@@ -49,8 +49,8 @@
 
 # 118
 # warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
-# user = input()
-# if user in warn_investment_list:
+# 종목 = input("종목명: ")
+# if 종목 in warn_investment_list:
 #     print('투자 경고 종목입니다')
 # else:
 #     print("투자 경고 종목이 아닙니다.")
